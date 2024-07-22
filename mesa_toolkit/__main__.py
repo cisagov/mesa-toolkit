693d88db46e18a5a6242eca6ef393dc57a73bd33
m1j0 <40894613+m1j09830@users.noreply.github.com>
2024-07-22 14:40:13 -0400

    Remove unnecessary functions that exceed the scope of MESA project

--- mesa_toolkit/__main__.py
+++ mesa_toolkit/__main__.py
@@ -22,9 +22,7 @@ class Operations(str, Enum):
     aquatone = "aquatone"
     encryption_check = "encryption_check"
     default_logins = "default_logins"
-    domain_enum = "domain_enum"
     smb_signing_check = "smb_signing_check"
-    pass_policy_check = "pass_policy_check"
     vuln_scans = "vuln_scans"
     all_checks = "all_checks"
     report_generator = "report_generator"
@@ -41,18 +39,6 @@ def main(
     exclude_file: Path = typer.Option(
         None, '--exclude-file', '-e', exists=True, file_okay=True, dir_okay=False,
         readable=True, resolve_path=True, help='Set exclude file'),
-    domain: str = typer.Option(
-        None, '--domain', '-d', help='Set domain for domain enumeration'),
-    dc_ip: str = typer.Option(
-        None, '--domain-controller', '-dc', help='Set domain controller IP address for domain enumeration'),
-    domain_user: str = typer.Option(
-        None, '--domain-username', '-du', help='Set username for domain enumeration'),
-    domain_pass: str = typer.Option(
-        None, '--domain-password', '-dp', help='Set password for domain enumeration'),
-    neo4j_user: str = typer.Option(
-        None, '--neo4j-username', '-nu', help='Set username for neo4j database'),
-    neo4j_pass: str = typer.Option(
-        None, '--neo4j-password', '-np', help='Set password for neo4j database'),
     customer_name: str = typer.Option(
         None, '--customer-name', '-cn', help='Set customer long name for report generator'),
     customer_initials: str = typer.Option(
@@ -88,14 +74,8 @@ def main(
     if operation == Operations.default_logins:
         default_logins(project_name, input_file, exclude_file)
 
-    if operation == Operations.domain_enum:
-        domain_enum(project_name, domain, dc_ip, domain_user, domain_pass, neo4j_user, neo4j_pass)
-
     if operation == Operations.smb_signing_check:
         smb_signing_check(project_name, input_file, exclude_file)
-
-    if operation == Operations.pass_policy_check:
-        pass_policy_check(project_name, dc_ip, domain_user, domain_pass)
 
     if operation == Operations.vuln_scans:
         vuln_scans(project_name, input_file, exclude_file)
@@ -105,10 +85,6 @@ def main(
 
     if operation == Operations.report_generator:
         report_generator(project_name, customer_name, customer_initials)
-
-# The following commented section can be used once the domain functions have been tested more fully.
-#    if operation == Operations.all_checks:
-#        all_checks(project_name, domain, dc_ip, domain_user, domain_pass, neo4j_user, neo4j_pass, input_file, exclude_file)
 
 if __name__ == '__main__':
     app(prog_name='MESA-Toolkit')
